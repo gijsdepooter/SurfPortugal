4ca46abdb2842337cbc8de4941260e35c8e55caa
Gijs <gjd19@ic.ac.uk>
2023-09-26 14:15:37 +0100

    Initial commit

--- SurfPortalProject/SurfPortalProject/settings.py
+++ SurfPortalProject/SurfPortalProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication settings
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',  # Use Django's default authentication backend
+)
+
+LOGIN_URL = 'login'  # URL to the login view
+LOGOUT_URL = 'logout'  # URL to the logout view
+LOGIN_REDIRECT_URL = 'home'  # URL to redirect to after login
--- SurfPortalProject/authapp/views.py
+++ SurfPortalProject/authapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired post-registration redirect URL
+    else:
+        form = UserCreationForm()
+    return render(request, 'authapp/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired post-login redirect URL
+    else:
+        form = AuthenticationForm()
+    return render(request, 'authapp/login.html', {'form': form})
